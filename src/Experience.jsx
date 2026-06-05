6b5414e0ae12d448aea098f21578e42dbccd9b1c
Varun2976 <vsnair2976@gmail.com>
2026-06-05 13:33:58 +0530

    Added hovering boxes needed to add

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,12 @@
 import AnimatedList from "./AnimatedList";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Button } from "@/components/ui/button"
+
+const HOVER_CARD_SIDES = ["top", "right", "bottom", "left"];
 
 const experience = [
   {
@@ -24,13 +32,24 @@ function Experience() {
 
       <hr className="mb-6 border-cyan-300/30" />
         <div className="text-green-500 font-mono">
-            <AnimatedList
-        items={experienceItems}
-        onItemSelect={(item, index) => console.log(item, index)}
-        showGradients
-        enableArrowNavigation
-        displayScrollbar
-      />
+          <div className="flex flex-wrap justify-center gap-2">
+      {HOVER_CARD_SIDES.map((side) => (
+        <HoverCard key={side} openDelay={100} closeDelay={100}>
+          <HoverCardTrigger asChild>
+            <Button variant="outline" className="capitalize">
+              {side}
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent side={side}>
+            <div className="flex flex-col gap-1">
+              <h4 className="font-medium">Hover Card</h4>
+              <p>This hover card appears on the {side} side of the trigger.</p>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+      ))}
+    </div>
+           
         </div>
       
     </section>
